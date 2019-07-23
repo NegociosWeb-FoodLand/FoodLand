f5aff187669157820a48fd08744a3ab8a1b889e5
marcoselisorto <marcoselisorto@hotmail.com>
2019-07-23 16:34:28 -0600

    Creación de instancia y puerto de escucha del servidor (punto de entrada)

---
+++ index.js
@@ -0,0 +1,8 @@
+//- Importar los módulos de express
+const express = require('express');
+
+//- creamos una instancia de express
+ const app = express();
+
+ // iniciar el servidor en un puerto de escucha; en este caso puerto: 9000
+ app.listen(9000);
